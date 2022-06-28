d5f5e6b493f715d70a03276140316934217b90bf
unknown <daniel.preuss93@gmail.com>
2022-06-28 13:25:57 +0200

    Changed std::map to std::unordered_map

--- DVR/src/gl_debug.h
+++ DVR/src/gl_debug.h
@@ -2,16 +2,16 @@
 
 #include <glad/glad.h>
 
-#include <map>
+#include <unordered_map>
 
 
 namespace gl_debug {
 
 #ifndef NDEBUG
-	std::map<GLenum, const char*> debug_type{ {GL_DEBUG_TYPE_ERROR, "Error"}, {GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR, "Deprecated"}, {GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR, "Undefined"},
+	std::unordered_map<GLenum, const char*> debug_type{ {GL_DEBUG_TYPE_ERROR, "Error"}, {GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR, "Deprecated"}, {GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR, "Undefined"},
 		{GL_DEBUG_TYPE_PORTABILITY, "Portability"}, {GL_DEBUG_TYPE_PERFORMANCE, "Performance"}, {GL_DEBUG_TYPE_MARKER, "Marker"}, {GL_DEBUG_TYPE_PUSH_GROUP, "Push Group"},
 		{GL_DEBUG_TYPE_POP_GROUP, "Pop Group"}, {GL_DEBUG_TYPE_OTHER, "Other"} };
-	std::map<GLenum, const char*> debug_severity{ {GL_DEBUG_SEVERITY_HIGH, "High"}, {GL_DEBUG_SEVERITY_MEDIUM, "Medium"}, 
+	std::unordered_map<GLenum, const char*> debug_severity{ {GL_DEBUG_SEVERITY_HIGH, "High"}, {GL_DEBUG_SEVERITY_MEDIUM, "Medium"}, 
 												{GL_DEBUG_SEVERITY_LOW, "Low"}, {GL_DEBUG_SEVERITY_NOTIFICATION, "Notification"} };
 	void GLAPIENTRY message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* user_param) {
 		fprintf(stderr, "GL Callback: type = %s, severity = %s, message = %s\n", debug_type[type], debug_severity[severity], message);
